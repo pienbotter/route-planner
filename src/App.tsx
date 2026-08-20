1e115184120f9c1004d7730e506a6c208e9b1c39
pienbotter <pienbotter@gmail.com>
2026-08-20 13:24:33 +0200

    Add MapLibre map

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useRef } from "react";
+import { Map, setWorkerUrl } from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url";
+
+setWorkerUrl(workerUrl);
+
 function App() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!mapContainer.current) return;
+
+    const map = new Map({
+      container: mapContainer.current,
+      center: [4.9, 52.37],
+      zoom: 11,
+      style: "https://demotiles.maplibre.org/style.json",
+    });
+
+    return () => map.remove();
+  }, []);
+
   return (
     <main>
-      <h1>Route Planner</h1>
-      <p>Build your perfect route.</p>
+      <div
+        ref={mapContainer}
+        style={{
+          width: "100vw",
+          height: "100vh",
+        }}
+      />
     </main>
   );
 }
